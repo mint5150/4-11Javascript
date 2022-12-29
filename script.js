91c744307dbb2345245c816a68938495a7715faa
mint5150 <musia.home@gmail.com>
2022-12-29 13:39:56 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+for (var i = 1; i <= 10; i++) {
+  console.log("ご予約" + i + "人目");
+  };
+
+var tools = ["ハサミ", "ペンチ", "脚立"];
+for (index in tools) {
+  console.log(tools[index]);
+};
+
+var staffs = [
+{ name: "絹枝", age: 57, subject: "エリアマネージャー", },
+{ name: "春太郎", age: 33, subject: "料理長", },
+{ name: "かな子", age: 22, subject: "ホール", }]
+for (staffs of staffs) {
+  console.log(staffs);
+};
