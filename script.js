3f844e9dbee6b64076084932173c05d194d6ad6a
mint5150 <musia.home@gmail.com>
2023-01-04 02:45:28 +0900

    master

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 for (var i = 1; i <= 10; i++) {
   console.log("ご予約" + i + "人目");
-  };
+};
 
 var tools = ["ハサミ", "ペンチ", "脚立"];
 for (index in tools) {
@@ -8,9 +8,10 @@ for (index in tools) {
 };
 
 var staffs = [
-{ name: "絹枝", age: 57, subject: "エリアマネージャー", },
-{ name: "春太郎", age: 33, subject: "料理長", },
-{ name: "かな子", age: 22, subject: "ホール", }]
+  { name: "絹枝", age: 57, subject: "エリアマネージャー", },
+  { name: "春太郎", age: 33, subject: "料理長", },
+  { name: "かな子", age: 22, subject: "ホール", }
+]
 for (staffs of staffs) {
   console.log(staffs);
 };
